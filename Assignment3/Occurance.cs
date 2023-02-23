db5e5e6f42008a8d1b9f5b366392aec8edf927f7
Harish0709 <113748844+Harish0709@users.noreply.github.com>
2023-02-23 21:19:30 +0530

    Add files via upload

---
+++ Assignment3/Occurance.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment31
+{
+    class Occurance
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Enter a string:");
+            string inp_string = Console.ReadLine();
+
+            Console.WriteLine("Enter the letter to count in the given string:");
+            char letter = Console.ReadLine()[0];
+
+            int cnt = CounttheOccurrences(inp_string, letter);
+            Console.WriteLine($"The letter {letter} appears {cnt} times in the string.");
+            Console.Read();
+        }
+
+        public static int CounttheOccurrences(string inp_string, char letter)
+        {
+            int cnt = 0;
+
+            foreach (char i in inp_string)
+            {
+                if (i == letter)
+                {
+                    cnt++;
+                }
+            }
+
+            return cnt;
+            
+        }
+    }
+}
+
---
+++ Assignment3/uppercaseprogram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3
+{
+    class Program123
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your first name:");
+            string fName = Console.ReadLine();
+
+            Console.WriteLine("Enter your last name:");
+            string lName = Console.ReadLine();
+
+            Program123 pro = new Program123();
+            Display(fName.ToUpper(),lName.ToUpper());
+        }
+
+        public static void Display(string fName, string lName)
+        {
+            Console.WriteLine(fName);
+            Console.WriteLine(lName);
+            Console.Read();
+        }
+    }
+}
+    
