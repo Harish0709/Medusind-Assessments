d7c302c79cc4e208883f2aa817ece6811aeeb6de
Harish0709 <113748844+Harish0709@users.noreply.github.com>
2023-02-27 20:49:28 +0530

    Add files via upload

---
+++ Assignment5/Dayscholar.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    interface IStudent
+    {
+        int Stud_Id { get; set; }
+        string Name { get; set; }
+        void ShowDetails();
+    }
+
+    class DayScholar : IStudent
+    {
+        public int Stud_Id { get; set; }
+        public string Name { get; set; }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Day Scholar Details");
+            Console.WriteLine("Student ID: " + Stud_Id);
+            Console.WriteLine("Student Name: " + Name);
+            Console.WriteLine();
+        }
+    }
+
+    class Resident : IStudent
+    {
+        public int Stud_Id { get; set; }
+        public string Name { get; set; }
+
+        public void ShowDetails()
+        {
+            Console.WriteLine("---------------------------------");
+            Console.WriteLine("Resident Details");
+            Console.WriteLine("Student ID: " +Stud_Id);
+            Console.WriteLine("Student Name: " +Name);
+            Console.WriteLine();
+        }
+    }
+
+    class Interface
+    {
+        public static void Main()
+        {
+            DayScholar d = new DayScholar();
+
+            Console.WriteLine("Enter The Student Id :");
+            d.Stud_Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter The Student Name :");
+            d.Name = Console.ReadLine();
+
+            d.ShowDetails();
+
+            Resident r = new Resident();
+
+            Console.WriteLine("Enter The Student Id :");
+            r.Stud_Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter The Student Name:");
+            r.Name = Console.ReadLine();
+
+            r.ShowDetails();
+
+            Console.Read();
+        }
+    }
+}
---
+++ Assignment5/student.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+    public abstract class Student
+    {
+        public string Name;
+        public int Stud_Id;
+        public double Grade;
+
+        public abstract bool Ispassed(double grade);
+    }
+
+    class Undergraduate : Student
+    {
+        public override bool Ispassed(double grade)
+        {
+            if (grade > 70.0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+
+    class Graduate : Student
+    {
+        public override bool Ispassed(double grade)
+        {
+            if (grade > 80.0)
+            {
+                return true;
+
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+    }
+
+    class student
+    {
+        static void Main(string[] args)
+        {
+            
+            Undergraduate u = new Undergraduate();
+
+            
+            Console.Write("Enter the name of the undergraduate student: ");
+            u.Name = Console.ReadLine();
+
+            Console.Write("Enter the student ID: ");
+            u.Stud_Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the grade for the course: ");
+            u.Grade = Convert.ToDouble(Console.ReadLine());
+
+            
+            bool status = u.Ispassed(u.Grade);
+            Console.WriteLine("Status for {0} with ID {1}: {2}", u.Name, u.Stud_Id, status);
+
+            
+            Graduate g = new Graduate();
+
+           
+            Console.Write("Enter the name of the graduate student: ");
+            g.Name = Console.ReadLine();
+
+            Console.Write("Enter the student ID: ");
+            g.Stud_Id = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Enter the grade for the course: ");
+            g.Grade = Convert.ToDouble(Console.ReadLine());
+
+           
+            status = g.Ispassed(g.Grade);
+            Console.WriteLine("Status for {0} with ID {1}: {2}", g.Name, g.Stud_Id, status);
+
+            Console.ReadLine();
+        }
+    }
+
+}
